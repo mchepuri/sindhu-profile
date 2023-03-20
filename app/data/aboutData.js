d506ad8e8464009add27aa8fa48df15b9f3d1b81
mchepuri <muralimanohar.chepuri.contractor@move.com>
2023-03-19 21:40:27 -0700

    total hours of expereince updates

--- app/data/aboutData.js
+++ app/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "Hello! My name is Sindhusha Gundlapally. I'm a doctor from India specialized in womens health care!, Pursuing medicine in U.S.A",
-    description2: "I did my M.B.B.S  ( Bachelors in medicine and Bachelors in surgery) at Osmania Medical College in Hyderabad , And PG Diploma in Gynelocogy at K.I.M.S. Have got a chance to work in an esteemed hospital as a Sr Resident when i performed over 400 surgeries and 3000 hours of clinical experience. Currently looking to purse a degree that will complement my ineterest and my medical career in U.S.A",
+    description2: "I did my M.B.B.S  ( Bachelors in medicine and Bachelors in surgery) at Osmania Medical College in Hyderabad , And PG Diploma in Gynelocogy at K.I.M.S. Have got a chance to work in an esteemed hospital as a Sr Resident when i performed over 400 surgeries and 6000 hours of clinical experience. Currently looking to purse a degree that will complement my ineterest and my medical career in U.S.A",
     image: 2
 }
