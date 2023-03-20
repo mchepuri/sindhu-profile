e5f69d4fe0ad49a9596f5f39944fb8f408fd7fcc
mchepuri <muralimanohar.chepuri.contractor@move.com>
2023-03-19 21:36:02 -0700

    Webpack config change from dist to build

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
     output:{
       filename: 'index.bundle.js',
       chunkFilename: '[name].bundle.js',
-      path: path.resolve(__dirname , './dist')
+      path: path.resolve(__dirname , './build')
       },
     module:{
         rules:[{
